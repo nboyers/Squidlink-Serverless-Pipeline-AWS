7d229947cd567252193da8ca079db541a7354852
nboyers <frenttv@gmail.com>
2022-07-21 13:58:39 -0400

    create unit tests

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
+
 # Squidlink-Serverless-Pipeline-AWS
+
+
+This sample application is an example of Node JS application to demonstrate CI/CD Pipeline and the capabilities inside AWS CodeBuild.
+
+
+## Release Notes
+
+- Create sample unit test
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
---
+++ test/index.test.js
@@ -0,0 +1,47 @@
+
+  const fizz_buzz = require('../src/index.js');
+
+  /**
+   * This assert will pass 
+   */
+  describe("FizzBuzz", () => {
+      test('[3] should result in "fizz"', () => {
+        expect(fizz_buzz([3])).toBe('fizz');
+      });
+  
+      test('[5] should result in "buzz"', () => {
+        expect(fizz_buzz([5])).toBe('buzz');
+      });
+  
+      test('[15] should result in "fizzbuzz"', () => {
+        expect(fizz_buzz([15])).toBe('fizzbuzz');
+      });
+  
+      test('[1,2,3] should result in "1, 2, fizz"', () => {
+        expect(fizz_buzz([3])).toBe('fizz');
+      });
+  
+  });
+
+ /**
+  * ! This assert is suppose to fail !
+  */
+ 
+  describe("FizzBuzz", () => {
+    test('[3] should result in "fizz"', () => {
+      expect(fizz_buzz([2])).toBe('fizz');
+    });
+
+    test('[5] should result in "buzz"', () => {
+      expect(fizz_buzz([15])).toBe('buzz');
+    });
+
+    test('[15] should result in "fizzbuzz"', () => {
+      expect(fizz_buzz([5])).toBe('fizzbuzz');
+    });
+
+    test('[1,2,3] should result in "1, 2, fizz"', () => {
+      expect(fizz_buzz([15])).toBe('fizz');
+    });
+
+});
