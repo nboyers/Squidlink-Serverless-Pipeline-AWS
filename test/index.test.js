c57bf001cc04e2a71e29b78b760867c0d4ad409a
nboyers <frenttv@gmail.com>
2022-07-28 07:50:35 -0400

    Cloud Test

--- test/index.test.js
+++ test/index.test.js
@@ -27,21 +27,21 @@
   * ! This assert is suppose to fail !
   */
  
-//   describe("FizzBuzz", () => {
-//     test('[3] should result in "fizz"', () => {
-//       expect(fizz_buzz([2])).toBe('fizz');
-//     });
+  describe("FizzBuzz", () => {
+    test('[3] should result in "fizz"', () => {
+      expect(fizz_buzz([2])).toBe('fizz');
+    });
 
-//     test('[5] should result in "buzz"', () => {
-//       expect(fizz_buzz([15])).toBe('buzz');
-//     });
+    test('[5] should result in "buzz"', () => {
+      expect(fizz_buzz([15])).toBe('buzz');
+    });
 
-//     test('[15] should result in "fizzbuzz"', () => {
-//       expect(fizz_buzz([5])).toBe('fizzbuzz');
-//     });
+    test('[15] should result in "fizzbuzz"', () => {
+      expect(fizz_buzz([5])).toBe('fizzbuzz');
+    });
 
-//     test('[1,2,3] should result in "1, 2, fizz"', () => {
-//       expect(fizz_buzz([15])).toBe('fizz');
-//     });
+    test('[1,2,3] should result in "1, 2, fizz"', () => {
+      expect(fizz_buzz([15])).toBe('fizz');
+    });
 
-// });
+});
